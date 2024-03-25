aed11e1bf08a90c6a1c29de361d097a459b2bbea
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-25 23:15:23 +0200

    sample data added for testing

---
+++ client/src/constants/sampleData.js
@@ -0,0 +1,16 @@
+export const sampleChats = [
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "Subin Doe",
+    _id: "1",
+    groupChat: false,
+    members: ["1", "2"],
+  },
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "Bimala Rai",
+    _id: "2",
+    groupChat: true,
+    members: ["1", "2"],
+  },
+];
