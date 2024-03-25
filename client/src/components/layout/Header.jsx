0c1b2b1811e6d5e74b893d1e886e4dc58a5c6f3d
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-25 23:12:03 +0200

    Icons added and implemented onClick handlers

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,25 +1,55 @@
 import {
   AppBar,
+  Backdrop,
   Box,
   IconButton,
   Toolbar,
   Tooltip,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { Suspense, useState } from "react";
 import { orange } from "../../constants/color";
 import {
   Add as AddIcon,
   Menu as MenuIcon,
   Search as SearchIcon,
+  Group as GroupIcon,
+  Logout as LogoutIcon,
+  Notifications as NotificationsIcon,
 } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
+const SearchDialog = React.lazy(() => import("../specific/Search"));
+const NotificationDialog = React.lazy(() =>
+  import("../specific/Notifications")
+);
+const NewGroupDialog = React.lazy(() => import("../specific/NewGroup"));
 
 const Header = () => {
-  const handleMobile = () => {};
-  const openSearchDialog = () => {
-    console.log("openSearchDialog");
+  const navigate = useNavigate();
+
+  const [isMobile, setIsMobile] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
+  const [isNewGroup, setIsNewGroup] = useState(false);
+  const [isNotification, setIsNotification] = useState(false);
+
+  const handleMobile = () => {
+    setIsMobile((prev) => !prev);
   };
-  const openNewGroup = () => {};
+  const openSearch = () => {
+    setIsSearch((prev) => !prev);
+  };
+  const openNewGroup = () => {
+    setIsNewGroup((prev) => !prev);
+  };
+  const openNotification = () => {
+    setIsNotification((prev) => !prev);
+  };
+  const navigateToGroup = () => {
+    navigate("/groups");
+  };
+
+  const logoutHandler = () => {};
 
   return (
     <>
@@ -50,23 +80,62 @@ const Header = () => {
             </Box>
             <Box sx={{ flexGrow: 1 }} />
             <Box>
-              <IconButton
-                color="inherit"
-                size="large"
-                onClick={openSearchDialog}
-              >
-                <SearchIcon />
-              </IconButton>
-              <Tooltip title="New Group">
-                <IconButton color="inherit" size="large" onClick={openNewGroup}>
-                  <AddIcon />
-                </IconButton>
-              </Tooltip>
+              <IconBtn
+                title="Search"
+                icon={<SearchIcon />}
+                onClick={openSearch}
+              />
+              <IconBtn
+                title="New Group"
+                icon={<AddIcon />}
+                onClick={openNewGroup}
+              />
+              <IconBtn
+                title="Manage Groups"
+                icon={<GroupIcon />}
+                onClick={navigateToGroup}
+              />
+              <IconBtn
+                title="Notifications"
+                icon={<NotificationsIcon />}
+                onClick={openNotification}
+              />
+              <IconBtn
+                title="Logout"
+                icon={<LogoutIcon />}
+                onClick={logoutHandler}
+              />
             </Box>
           </Toolbar>
         </AppBar>
       </Box>
+
+      {isSearch && (
+        <Suspense fallback={<Backdrop open />}>
+          <SearchDialog />
+        </Suspense> // suspense is used to lazy load the component and backdrop is used to show a loading screen with a faded black background
+      )}
+      {isNotification && (
+        <Suspense fallback={<Backdrop open />}>
+          <NotificationDialog />
+        </Suspense>
+      )}
+      {isNewGroup && (
+        <Suspense fallback={<Backdrop open />}>
+          <NewGroupDialog />
+        </Suspense>
+      )}
     </>
+  );
+};
+
+const IconBtn = ({ title, icon, onClick }) => {
+  return (
+    <Tooltip title={title}>
+      <IconButton color="inherit" size="large" onClick={onClick}>
+        {icon}
+      </IconButton>
+    </Tooltip>
   );
 };
 
