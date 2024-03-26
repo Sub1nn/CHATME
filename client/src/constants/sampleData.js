0e91f8f0186896d009ed2cd281e2bb2eabeea21d
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-26 20:57:31 +0200

    more sample data added for testing

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -14,3 +14,32 @@ export const sampleChats = [
     members: ["1", "2"],
   },
 ];
+
+export const sampleUsers = [
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "Subin Doe",
+    _id: "1",
+  },
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "Bimala Rai",
+    _id: "2",
+  },
+];
+export const sampleNotifications = [
+  {
+    sender: {
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      name: "Subin Doe",
+    },
+    _id: "1",
+  },
+  {
+    sender: {
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      name: "Subin Doe",
+    },
+    _id: "2",
+  },
+];
