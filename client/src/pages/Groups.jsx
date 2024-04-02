fb4c73251768d70d6b6c605077e886b33aeb8849
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-02 23:22:15 +0300

    Groups designed

--- client/src/pages/Groups.jsx
+++ client/src/pages/Groups.jsx
@@ -1,7 +1,251 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Drawer,
+  Grid,
+  IconButton,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import {
+  Done as DoneIcon,
+  Edit as EditIcon,
+  KeyboardBackspace as KeyboardBackspaceIcon,
+  Menu as MenuIcon,
+} from "@mui/icons-material";
+import React, { useEffect } from "react";
+import { matBlack } from "../constants/color";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "../components/styles/StyledComponents";
+import { memo } from "react";
+import AvatarCard from "../components/shared/AvatarCard";
+import { sampleChats } from "../constants/sampleData";
 
 const Groups = () => {
-  return <div>Groups</div>;
+  const chatId = useSearchParams()[0].get("group");
+  const navigate = useNavigate();
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const [groupName, setGroupName] = useState("");
+
+  const [groupNameUpdatedValue, setGroupNameUpdatedValue] = useState("");
+
+  const navigateBack = () => {
+    navigate("/");
+  };
+  const handleMobile = () => {
+    setIsMobileMenuOpen((prev) => !prev);
+  };
+
+  const handleMobileClose = () => setIsMobileMenuOpen(false);
+
+  const UpdateGroupName = () => {
+    setIsEdit(false);
+  };
+
+  useEffect(() => {
+    setGroupName(`Group Name ${chatId}`);
+    setGroupNameUpdatedValue(`Group Name ${chatId}`);
+    return () => {
+      setGroupName("");
+      setGroupNameUpdatedValue("");
+      setIsEdit(false);
+    };
+  }, [chatId]);
+
+  const IconBtns = (
+    <>
+      <Box
+        sx={{
+          display: {
+            xs: "block",
+            sm: "none",
+            position: "fixed",
+            top: "1rem",
+            right: "1rem",
+          },
+        }}
+      >
+        <IconButton onClick={handleMobile}>
+          <MenuIcon />
+        </IconButton>
+      </Box>
+      <Tooltip title="back">
+        <IconButton
+          sx={{
+            position: "absolute",
+            top: "2rem",
+            left: "2rem",
+            bgcolor: matBlack,
+            color: "white",
+            ":hover": {
+              bgcolor: "rgba(0, 0, 0, 0.7)",
+            },
+          }}
+          onClick={navigateBack}
+        >
+          <KeyboardBackspaceIcon />
+        </IconButton>
+      </Tooltip>
+    </>
+  );
+
+  const GroupName = (
+    <Stack
+      direction={"row"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      spacing={"1rem"}
+      padding={"3rem"}
+    >
+      {isEdit ? (
+        <>
+          <TextField
+            value={groupNameUpdatedValue}
+            onChange={(e) => setGroupNameUpdatedValue(e.target.value)}
+          />
+          <IconButton onClick={UpdateGroupName}>
+            <DoneIcon />
+          </IconButton>
+        </>
+      ) : (
+        <>
+          <Typography variant={"h4"}>{groupName}</Typography>
+          <IconButton onClick={() => setIsEdit(true)}>
+            <EditIcon />
+          </IconButton>
+        </>
+      )}
+    </Stack>
+  );
+
+  const ButtonGroup = (
+    <Stack
+      direction={{ xs: "column-reverse", sm: "row" }}
+      spacing={"1rem"}
+      p={{
+        xs: "0",
+        sm: "1rem",
+        md: "1rem 4rem",
+      }}
+    >
+      <Button>Delete Group</Button>
+      <Button>Add member</Button>
+    </Stack>
+  );
+
+  return (
+    <Grid container height={"100vh"}>
+      <Grid
+        item
+        sx={{
+          display: {
+            xs: "none",
+            sm: "block",
+          },
+        }}
+        sm={4}
+        bgcolor={"bisque"}
+      >
+        <GroupsList myGroups={sampleChats} chatId={chatId} />
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          position: "relative",
+          padding: "1rem 3rem",
+        }}
+      >
+        {IconBtns}
+        {groupName && (
+          <>
+            {GroupName}
+            <Typography
+              margin={"2rem"}
+              alignSelf={"flex-start"}
+              variant="body1"
+            >
+              Members
+            </Typography>
+            <Stack
+              maxWidth={"45rem"}
+              width={"100%"}
+              boxSizing={"border-box"}
+              padding={{
+                xs: "0",
+                sm: "1rem",
+                md: "1rem 4rem",
+              }}
+              spacing={"2rem"}
+              bgcolor={"#f5f5f5"}
+              height={"50vh"}
+              overflow={"auto"}
+            >
+              {/* Members List */}
+            </Stack>
+            {ButtonGroup}
+          </>
+        )}
+      </Grid>
+      <Drawer
+        sx={{
+          display: {
+            xs: "block",
+            sm: "none",
+          },
+        }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileClose}
+      >
+        <GroupsList myGroups={sampleChats} chatId={chatId} w={"50vw"} />
+      </Drawer>
+    </Grid>
+  );
 };
+
+const GroupsList = ({ w = "100%", myGroups = [], chatId }) => (
+  <Stack width={w}>
+    {myGroups.length > 0 ? (
+      myGroups.map((group) => (
+        <GroupListItem group={group} chatId={chatId} key={group._id} />
+      ))
+    ) : (
+      <Typography textAlign={"center"} padding={"1rem"}>
+        No groups
+      </Typography>
+    )}
+  </Stack>
+);
+
+const GroupListItem = memo(({ group, chatId }) => {
+  const { name, avatar, _id } = group;
+  return (
+    <Link
+      to={`?group=${_id}`}
+      onClick={(e) => {
+        if (chatId === _id) {
+          e.preventDefault();
+        }
+      }}
+    >
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        <AvatarCard avatar={avatar} />
+        <Typography>{name}</Typography>
+      </Stack>
+    </Link>
+  );
+});
 
 export default Groups;
