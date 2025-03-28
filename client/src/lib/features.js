d986e52b47cc5d9e8b3f2fd5a936901065be08be
Subin Khatiwada <Subinkhatiwada@gmail.com>
2025-03-28 14:34:30 +0200

    new features like image transformation and retrive storage data added

--- client/src/lib/features.js
+++ client/src/lib/features.js
@@ -1,35 +1,53 @@
+import moment from "moment";
+
 const fileFormat = (url = "") => {
   const fileExt = url.split(".").pop();
-  if (fileExt === "mp4" || fileExt === "webm" || fileExt === "ogg") {
+
+  if (fileExt === "mp4" || fileExt === "webm" || fileExt === "ogg")
     return "video";
-  } else if (fileExt === "mp3" || fileExt === "wav") {
-    return "audio";
-  } else if (fileExt === "pdf") {
-    return "pdf";
-  } else if (fileExt === "doc" || fileExt === "docx") {
-    return "doc";
-  } else if (fileExt === "xls" || fileExt === "xlsx") {
-    return "xls";
-  } else if (fileExt === "ppt" || fileExt === "pptx") {
-    return "ppt";
-  } else if (fileExt === "zip" || fileExt === "rar") {
-    return "archive";
-  } else if (fileExt === "txt") {
-    return "txt";
-  } else if (
+
+  if (fileExt === "mp3" || fileExt === "wav") return "audio";
+  if (
+    fileExt === "png" ||
     fileExt === "jpg" ||
     fileExt === "jpeg" ||
-    fileExt === "png" ||
     fileExt === "gif"
-  ) {
+  )
     return "image";
-  } else {
-    return "file";
-  }
+
+  return "file";
 };
 
+// https://res.cloudinary.com/dj5q966nb/image/upload/dpr_auto/w_200/v1710344436/fafceddc-2845-4ae7-a25a-632f01922b4d.png
+
+// /dpr_auto/w_200
 const transformImage = (url = "", width = 100) => {
-  return url;
+  const newUrl = url.replace("upload/", `upload/dpr_auto/w_${width}/`);
+
+  return newUrl;
 };
 
-export { fileFormat, transformImage };
+const getLast7Days = () => {
+  const currentDate = moment();
+
+  const last7Days = [];
+
+  for (let i = 0; i < 7; i++) {
+    const dayDate = currentDate.clone().subtract(i, "days");
+    const dayName = dayDate.format("dddd");
+
+    last7Days.unshift(dayName);
+  }
+
+  return last7Days;
+};
+
+const getOrSaveFromStorage = ({ key, value, get }) => {
+  if (get)
+    return localStorage.getItem(key)
+      ? JSON.parse(localStorage.getItem(key))
+      : null;
+  else localStorage.setItem(key, JSON.stringify(value));
+};
+
+export { fileFormat, transformImage, getLast7Days, getOrSaveFromStorage };
