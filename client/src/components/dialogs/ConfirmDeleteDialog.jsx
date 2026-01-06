fc9a995bff69071a555a5565dffa1fbe3ae647d0
Subin Khatiwada <Subinkhatiwada@gmail.com>
2026-01-06 23:32:09 +0200

    button click handler wrapper inside dialogAction wrapper

--- client/src/components/dialogs/ConfirmDeleteDialog.jsx
+++ client/src/components/dialogs/ConfirmDeleteDialog.jsx
@@ -11,17 +11,16 @@ import React from "react";
 const ConfirmDeleteDialog = ({ open, handleClose, deleteHandler }) => {
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Are you sure you want to delete this group?</DialogTitle>
+      <DialogTitle>Confirm Delete</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          Deleting this group will remove all the messages and members from this
-          group.
+          Are you sure you want to delete this group?
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>No</Button>
         <Button onClick={deleteHandler} color="error">
-          Delete
+          Yes
         </Button>
       </DialogActions>
     </Dialog>
